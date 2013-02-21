b6a13fc1703ed41698f0fe6a8d93ea95680f4c1b
Jorge Pedret <jorge@chloi.io>
2013-02-21 13:03:42 -0800

    Upgraded xml2js version to master edge. Bundled in node_modules. Tested it with three different XML files and working nicely. Reduced code to use 'cleanup' function. More fault tolerant.

--- example.js
+++ example.js
@@ -1,6 +1,11 @@
 var wpxml2json = require("./lib/wpxml2json");
 
-wpxml2json("assets/sample.xml", function (err, blog) {
+// You can try with these three different files:
+// - assets/sample.xml
+// - assets/sample-1.xml
+// - assets/sample-2.xml
+
+wpxml2json("assets/sample-2.xml", function (err, blog) {
   if (!err) {
     console.log("First category: ", blog.categories[0]);
   }
